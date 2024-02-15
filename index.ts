3ef93f43f39710c1db5582d2c9d824bad3e41c03
Theo Paris <theop@flowtr.dev>
2024-02-14 16:15:21 -0800

    feat: add archlinux package lookup

--- index.ts
+++ index.ts
@@ -1,59 +1,85 @@
 import { Hono } from "hono";
 
+const hasher = new Bun.CryptoHasher("sha1");
 const app = new Hono();
 
-app.get("/:packageQuery", (c) => {
+const arch = `x86_64`;
+
+const fetchArchPackageMeta = async (packageName: string, repo?: string) => {
+	const repository = repo ?? "core";
+	const mirror = `https://europe.mirror.pkgbuild.com/${repository}/os/${arch}`;
+	const archLinuxUri = `https://archlinux.org/packages/${repository}/${arch}/${packageName.replace(
+		"@archlinux/",
+		"",
+	)}/json`;
+	const response = await fetch(archLinuxUri);
+	const archLinuxJson = (await response.json()) as any;
+
+	const tarballUrl = `${mirror}/${archLinuxJson.filename}`;
+
+	const temp = await fetch(tarballUrl);
+	hasher.update(await temp.arrayBuffer());
+	const hash = hasher.digest("hex");
+	console.log(tarballUrl, hash);
+
+	const packageDescription: string = archLinuxJson.pkgdesc;
+	const packageVersion: string = `${archLinuxJson.pkgver}.${archLinuxJson.pkgrel}`;
+
+	return {
+		_attachments: {},
+		_id: packageName,
+		_rev: "00a603b57e0dd6e7474fe04030e715989634cb8ecf8118487ec6f72bee66d8f1",
+		author: {},
+		description: packageDescription,
+		"dist-tags": {
+			latest: packageVersion,
+		},
+		license: "GPL",
+		maintainers: [],
+		name: packageName,
+		readme: `# ${packageName}\n${packageDescription}`,
+		time: {
+			[packageVersion]: "2015-03-24T00:12:24.039Z",
+			created: "2015-03-24T00:12:24.039Z",
+			modified: "2015-03-24T00:12:24.039Z",
+		},
+		versions: {
+			[packageVersion]: {
+				_from: ".",
+				_id: `${packageName}@${packageVersion}`,
+				_npmUser: {},
+				_npmVersion: "2.7.0",
+				_shasum: hash,
+				author: {},
+				description: packageDescription,
+				directories: {},
+				dist: {
+					shasum: hash,
+					tarball: tarballUrl,
+				},
+				license: "GPL",
+				maintainers: [],
+				name: packageName,
+				scripts: {},
+				version: packageVersion,
+			},
+		},
+	};
+};
+
+app.get("/:packageQuery", async (c) => {
 	const { packageQuery } = c.req.param();
 
 	const packageName = packageQuery.includes(".")
 		? packageQuery.substring(0, packageQuery.indexOf(".") - 1)
 		: packageQuery;
 
-	return c.json({
-		_attachments: {},
-		_id: packageName,
-		_rev: "00a603b57e0dd6e7474fe04030e715989634cb8ecf8118487ec6f72bee66d8f1",
-		author: {
-		},
-		description: "Test package",
-		"dist-tags": {
-			"latest": "0.0.1"
-		},
-		license: "GPL",
-		maintainers: [],
-		name: packageName,
-		readme: `# ${packageName}`,
-		time: {
-			"0.0.1": "2015-03-24T00:12:24.039Z",
-			"created": "2015-03-24T00:12:24.039Z",
-			"modified": "2015-03-24T00:12:24.039Z",
-		},
-		versions: {
-			"0.0.1": {
-				"_from": ".",
-				"_id": `${packageName}@0.0.1`,
-				"_npmUser": {
-				},
-				"_npmVersion": "2.7.0",
-				"_shasum": "248b4b3c77a6582a8438fbc27511af379033b17d",
-				"author": {
-				},
-				"description": "Test package",
-				"directories": {},
-				"dist": {
-					"shasum": "248b4b3c77a6582a8438fbc27511af379033b17d",
-					"tarball": "https://europe.mirror.pkgbuild.com/core/os/x86_64/glibc-2.38-7-x86_64.pkg.tar.zst"
-				},
-				"license": "GPL",
-				"maintainers": [
-				],
-				"name": packageName,
-				"scripts": {
-				},
-				"version": "0.0.1"
-			}
-		},
-	});
+	if (!packageName.startsWith("@archlinux"))
+		return c.redirect(`https://registry.npmjs.org/${packageName}`);
+
+	const packageMetadata = await fetchArchPackageMeta(packageName);
+
+	return c.json(packageMetadata);
 });
 
 export default {
